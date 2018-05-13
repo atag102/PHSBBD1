dd6c19511092900b4ee3e3a68359d23fdd34f4f6
atag102 <atag102@gmail.com>
2018-05-12 23:44:41 -0400

    Se modifico ya que no eiste ninguna carpeta static

--- PHSBD1/settings.py
+++ PHSBD1/settings.py
@@ -123,11 +123,3 @@ STATIC_URL = '/static/'
 import dj_database_url  
 db_from_env = dj_database_url.config(conn_max_age=500)  
 DATABASES['default'].update(db_from_env)  
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (  
-    os.path.join(BASE_DIR, 'static'),
-)
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
